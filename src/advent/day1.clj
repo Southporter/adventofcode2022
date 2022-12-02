eee9e13f06e0194d19d745c2322754affcd7824b
Shem Sedrick <ssedrick1@gmail.com>
2022-12-02 10:21:12 -0600

    Day 1 complete

---
+++ src/advent/day1.clj
@@ -0,0 +1,42 @@
+(ns advent.day1
+  (:require [clojure.string :as str])
+  (:require [clojure.edn :as edn])
+)
+
+(defn get-input-as-str [file] 
+  (slurp 
+    (clojure.core/str "inputs/" file ".txt")
+  )
+)
+
+(defn count-calories [elf] 
+  (reduce + 
+          (map edn/read-string 
+               (clojure.string/split elf #"\n"))))
+
+(defn part1 [] 
+  (let 
+    [input (get-input-as-str "day1")]
+    (apply max (map count-calories (clojure.string/split input #"\n\n")
+    ))))
+
+(def calories (map count-calories (str/split (get-input-as-str "day1") #"\n\n")))
+(println calories)
+(def max-calories (apply clojure.core/max calories))
+(println max-calories)
+
+(defn max-n [n remaining & maxs] 
+  (let [m (apply max remaining)
+        r (filter #(< % m) remaining)]
+    (if (> n 0) (recur (- n 1) r (cons m maxs)) maxs)
+    ))
+
+
+(> 2 -1)
+(def l '(1 2 3 4 10 5 6 7))
+(def m (apply max l))
+(filter #(< % m) l)
+(max-n 2 l)
+
+(defn part2 [] 
+  (apply + (max-n 3 calories)))
---
+++ src/advent/main.clj
@@ -0,0 +1,11 @@
+(ns advent.main
+  "Advent of Code 2022"
+  ; (:require [clojure.repl :as repl])
+  ; (:require [clojure.stacktrace :as trace])
+  (:require [advent.day1 :as day1])
+)
+
+
+(println "Day 1 Star 1: " (day1/part1))
+(println "Day 1 Star 2: " (day1/part2))
+
